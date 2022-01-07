43d2ef91445b81f526cd07cd72eb52e1a644622b
NadiaKishchuk <nadia.kischyk2003@gmail.com>
2022-01-07 19:03:50 +0200

    load

---
+++ home14_3/DataBase.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.Sqlite;
+using System.Data;
+
+
+namespace home14_2
+{
+    class DataBase
+    {
+        public SqliteConnection connection;
+
+        public DataBase(in string connectionString)
+        {
+            connection = new SqliteConnection(connectionString);
+            connection.Open();
+        }
+
+        public void executeSQLite(string sqlCommand)
+        {
+            using (connection)
+            {
+                connection.Open();
+                SqliteCommand command = new SqliteCommand(sqlCommand, connection);
+                try
+                {
+                    command.ExecuteNonQuery();
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
+            }
+        }
+        //------------------------
+
+        public List<T> GetList<T>(string sqlCommand, Func<IDataRecord, T> creator)
+        {
+            connection.Open();
+            var list = new List<T>();
+            SqliteCommand command = new SqliteCommand(sqlCommand, connection);
+
+            using (SqliteDataReader reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    list.Add(creator(reader));
+                }
+            }
+            return list;
+        }
+    }
+
+    static class DataBaseExtensions
+    {
+        public static List<Product> GetProducts(this DataBase dataBase)
+        {
+            return dataBase.GetList("SELECT * from Products", Product.Create);
+        }
+        public static Product GetProductByNameLinq(this DataBase dataBase,string name)
+        {
+            return dataBase.GetProducts().Where(x => x.name == name).FirstOrDefault();
+        }
+        public static Product GetProductByNameDB(this DataBase dataBase, string name)
+        {
+            return dataBase.GetList($"SELECT * from Products WHERE Name like '{name}'", Product.Create).FirstOrDefault();
+        }
+
+    }
+     
+}
---
+++ home14_3/Product.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace home14_2
+{
+    class Product
+    {
+        public Product(string name, double cost, double weight, DateTime dateExpire, int amount, int discount = 0)
+           : this(name, cost, weight)
+        {
+            this.discount = discount;
+            this.DataExpire = dateExpire;
+            this.amount = amount;
+
+        }
+        public override string ToString()
+        {
+            return "class Product: name = " + name + ", cost = " + cost + ", weight= " + weight +
+                ", discount = " + discount + ", DataExpire = " + DataExpire + ", amount = " + amount + "\n\n";
+        }
+        public Product(string name, double cost, double weight)
+        {
+            this.name = name;
+            this.cost = cost;
+            this.weight = weight;
+
+        }
+        int discount;
+        public DateTime DataExpire;
+        
+        public string name;
+        protected double cost;
+        public int amount;
+        public double weight;
+        public static Product Create(IDataRecord data)
+        {
+            return new Product(data["Name"].ToString(), double.Parse(data["Cost"].ToString()),
+              double.Parse(data["Weight"].ToString()), DateTime.Parse(data["DateExpire"].ToString()), int.Parse(data["Amount"].ToString()), int.Parse(data["Discount"].ToString()));
+        }
+
+    }
+}
