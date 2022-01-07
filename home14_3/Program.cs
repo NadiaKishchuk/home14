69baf13cadb47c9bbf7c6d1f56c8358e900b1791
NadiaKishchuk <nadia.kischyk2003@gmail.com>
2022-01-07 18:57:15 +0200

    create

---
+++ home14_3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace home14_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataBase dataBase = new DataBase(@"Data Source=C:\Users\Надія\Desktop\Sigma c#\dataBases\Products.db;");
+            try
+            {
+                foreach (Product product in dataBase.GetProducts())
+                {
+                    Console.WriteLine(product);
+                }
+                Console.WriteLine(dataBase.GetProductByNameDB("Cheese"));
+                Console.WriteLine(dataBase.GetProductByNameLinq("Cheese"));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
